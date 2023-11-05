c58b0468b6ed7cdd8d4f6c0ed8677c9ceeeca338
b4b4r07 <b4b4r07@gmail.com>
2023-11-05 11:57:53 +0900

    Manage goroutine with errgroup

--- go.mod
+++ go.mod
@@ -5,6 +5,7 @@ go 1.20
 require (
 	github.com/barasher/go-exiftool v1.10.0
 	github.com/jessevdk/go-flags v1.5.0
+	golang.org/x/sync v0.5.0
 )
 
 require golang.org/x/sys v0.0.0-20220811171246-fbc7d0a398ab // indirect
--- main.go
+++ main.go
@@ -1,16 +1,17 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"os"
 	"path/filepath"
 	"sort"
-	"sync"
 	"time"
 
 	"github.com/barasher/go-exiftool"
 	"github.com/jessevdk/go-flags"
+	"golang.org/x/sync/errgroup"
 )
 
 type Option struct {
@@ -24,53 +25,86 @@ type Photo struct {
 	CreatedAt time.Time
 }
 
-func worker(inputs []string) <-chan Photo {
-	output := make(chan Photo)
-	var task sync.WaitGroup
+// func worker(files []string) <-chan Photo {
+// 	output := make(chan Photo)
+// 	eg := errgroup.Group{}
+// 	for _, file := range files {
+// 		f := file
+// 		eg.Go(func() error {
+// 			p, err := exif(f)
+// 			if err != nil {
+// 				return err
+// 			}
+// 			output <- p
+// 			return nil
+// 		})
+// 	}
+// 	// if err := ; err != nil {
+// 	//     log.Fatal(err)
+// 	// }
+//
+// 	go func() {
+// 		_ = eg.Wait()
+// 		close(output)
+// 	}()
+//
+// 	return output
+// }
 
-	for _, input := range inputs {
-		input := input
-		task.Add(1)
-		go func() {
-			defer task.Done()
-			p, err := exif(input)
+func worker2(ctx context.Context, files []string) ([]Photo, error) {
+	ch := make(chan Photo)
+	eg, ctx := errgroup.WithContext(ctx)
+
+	for _, file := range files {
+		file := file
+		eg.Go(func() error {
+			photo, err := exif(file)
 			if err != nil {
-				log.Println(err)
+				return err
 			}
-			output <- p
-		}()
+			fmt.Printf("Checking %s (time: %s)\n", photo.Name, photo.CreatedAt)
+			select {
+			case ch <- photo:
+			case <-ctx.Done():
+				return ctx.Err()
+			}
+			return nil
+		})
 	}
 
+	// https://devlights.hatenablog.com/entry/2020/03/10/112904
 	go func() {
-		task.Wait()
-		close(output)
+		_ = eg.Wait()
+		close(ch)
 	}()
 
-	return output
+	var photos []Photo
+	for photo := range ch {
+		photos = append(photos, photo)
+	}
+
+	// secondaly wait
+	return photos, eg.Wait()
 }
 
 func main() {
+	ctx := context.Background()
+
 	var opt Option
 	args, err := flags.Parse(&opt)
 	if err != nil {
 		panic(err)
 	}
 
-	var photos []Photo
-	ch := worker(args)
-	for photo := range ch {
-		fmt.Printf("Checking %s (time: %s)\n", photo.Name, photo.CreatedAt)
-		photos = append(photos, photo)
+	photos, err := worker2(ctx, args)
+	if err != nil {
+		panic(err)
 	}
-
-	// photos, err := exifBulk(args)
-	// if err != nil {
-	// 	panic(err)
-	// }
 
 	sort.Slice(photos, func(i, j int) bool {
 		return photos[i].CreatedAt.Before(photos[j].CreatedAt)
 	})
+
 	for i, photo := range photos {
 		newName := fmt.Sprintf("%s-%03d.%s",
 			photo.CreatedAt.Format("2006-01-02"),
@@ -79,6 +113,7 @@ func main() {
 		)
 		moveTo := filepath.Join(opt.ParentDir, newName)
 		fmt.Printf("Renaming %q to %q\n", photo.Path, moveTo)
+
 		if err := os.Rename(photo.Path, moveTo); err != nil {
 			log.Print(err)
 			continue
@@ -157,49 +192,49 @@ func exif(file string) (Photo, error) {
 	}, nil
 }
 
-func exifBulk(files []string) ([]Photo, error) {
-	et, err := exiftool.NewExiftool()
-	if err != nil {
-		return []Photo{}, err
-	}
-	defer et.Close()
-
-	var photos []Photo
-
-	fileInfos := et.ExtractMetadata(files...)
-	for _, fileInfo := range fileInfos {
-		if fileInfo.Err != nil {
-			return []Photo{}, err
-		}
-		filename, err := fileInfo.GetString("FileName")
-		if err != nil {
-			return []Photo{}, err
-		}
-		// Use it instead of DateTimeOriginal
-		dateTime, err := fileInfo.GetString("SubSecDateTimeOriginal")
-		if err != nil {
-			return []Photo{}, err
-		}
-		sourceFile, err := fileInfo.GetString("SourceFile")
-		if err != nil {
-			return []Photo{}, err
-		}
-		ext, err := fileInfo.GetString("FileTypeExtension")
-		if err != nil {
-			return []Photo{}, err
-		}
-		createdAt, err := time.Parse("2006:01:02 15:04:05.000-07:00", dateTime)
-		if err != nil {
-			return []Photo{}, err
-		}
-
-		photos = append(photos, Photo{
-			Name:      filename,
-			Path:      sourceFile,
-			Extension: ext,
-			CreatedAt: createdAt,
-		})
-	}
-
-	return photos, nil
-}
+// func exifBulk(files []string) ([]Photo, error) {
+// 	et, err := exiftool.NewExiftool()
+// 	if err != nil {
+// 		return []Photo{}, err
+// 	}
+// 	defer et.Close()
+//
+// 	var photos []Photo
+//
+// 	fileInfos := et.ExtractMetadata(files...)
+// 	for _, fileInfo := range fileInfos {
+// 		if fileInfo.Err != nil {
+// 			return []Photo{}, err
+// 		}
+// 		filename, err := fileInfo.GetString("FileName")
+// 		if err != nil {
+// 			return []Photo{}, err
+// 		}
+// 		// Use it instead of DateTimeOriginal
+// 		dateTime, err := fileInfo.GetString("SubSecDateTimeOriginal")
+// 		if err != nil {
+// 			return []Photo{}, err
+// 		}
+// 		sourceFile, err := fileInfo.GetString("SourceFile")
+// 		if err != nil {
+// 			return []Photo{}, err
+// 		}
+// 		ext, err := fileInfo.GetString("FileTypeExtension")
+// 		if err != nil {
+// 			return []Photo{}, err
+// 		}
+// 		createdAt, err := time.Parse("2006:01:02 15:04:05.000-07:00", dateTime)
+// 		if err != nil {
+// 			return []Photo{}, err
+// 		}
+//
+// 		photos = append(photos, Photo{
+// 			Name:      filename,
+// 			Path:      sourceFile,
+// 			Extension: ext,
+// 			CreatedAt: createdAt,
+// 		})
+// 	}
+//
+// 	return photos, nil
+// }
