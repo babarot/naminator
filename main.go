e6fec522b28892afd26f503ba177b7fb87f84b09
babarot <b4b4r07@gmail.com>
2025-02-04 03:04:06 +0900

    New ui as I expected

--- main.go
+++ main.go
@@ -6,10 +6,8 @@ import (
 	"fmt"
 	"io"
 	"io/fs"
-	"log"
 	"os"
 	"path/filepath"
-	"sort"
 	"strings"
 	"time"
 
@@ -37,8 +35,7 @@ type Option struct {
 	GroupByDate bool `short:"t" long:"group-by-date" description:"Create a date directory and classify the photos for each date" required:"false"`
 	GroupByExt  bool `short:"e" long:"group-by-ext" description:"Create an extension directory and classify the photos for each ext" required:"false"`
 
-	Clean     bool `short:"c" long:"clean" description:"Clean up directories after renaming" required:"false"`
-	WithIndex bool `long:"with-index" description:"Include index in a file name" required:"false"`
+	Clean bool `short:"c" long:"clean" description:"Clean up directories after renaming" required:"false"`
 
 	Version bool `short:"v" long:"version" description:"Show version"`
 }
@@ -62,7 +59,6 @@ func runMain() error {
 	ctx := context.Background()
 
 	var opt Option
-	// parser := flags.NewParser(&opt, flags.Default & ^flags.HelpFlag) // if remove default help flag
 	parser := flags.NewParser(&opt, flags.Default)
 	parser.Name = appName
 	parser.Usage = "[OPTIONS] [files... | dirs...]"
@@ -83,28 +79,62 @@ func runMain() error {
 		return fmt.Errorf("too few arguments")
 	}
 
-	// var allPhotos []Photo
 	images, err := getImages(args)
 	if err != nil {
 		return err
 	}
-	p := tea.NewProgram(newModel())
+	p := tea.NewProgram(newModel(len(images)))
+
+	var errs error
+	rename := func(photo Photo) error {
+		var newPath string
+		dest := opt.DestDir
+		if dest == "" {
+			dest = photo.Dir
+		}
+		if opt.GroupByDate {
+			dt := photo.CreatedAt.Format("2006-01-02")
+			dest = filepath.Join(dest, dt)
+		}
+		if opt.GroupByExt {
+			ext := getExt(photo.Path)
+			dest = filepath.Join(dest, ext)
+		}
+		newPath = filepath.Join(dest, fmt.Sprintf("%s.%s",
+			photo.CreatedAt.Format("2006-01-02_15-04-05"),
+			photo.Extension,
+		))
+		if opt.Dryrun {
+			p.Send(renameResultMsg{photo: photo, newPath: newPath, dryrun: true})
+			return nil
+		}
+		p.Send(renameResultMsg{photo: photo, newPath: newPath})
+		_ = os.MkdirAll(dest, 0755)
+		if err := os.Rename(photo.Path, newPath); err != nil {
+			// Use hashicorp/go-multierror instead of errors.Join (as of Go 1.20)
+			// because this one is pretty good in output format.
+			errs = multierror.Append(
+				errs,
+				fmt.Errorf("%s: failed to rename: %w", photo.Path, err))
+		}
+		return nil
+	}
 
 	ch := make(chan Photo, len(images))
 	eg, ctx := errgroup.WithContext(ctx)
 	for _, image := range images {
-		// walkDir can traverse dirs or files
 		image := image
 		eg.Go(func() error {
 			start := time.Now()
 			photo, err := analyzeExifdata(filepath.Dir(image), image)
-			if err != nil {
-				log.Print(fmt.Errorf("%s: failed to get EXIF data: %w", image, err))
-				return nil
-			}
 			select {
 			case ch <- photo:
-				p.Send(resultMsg{photo: photo, duration: duration(time.Since(start))})
+				p.Send(analyzeResultMsg{
+					photo:    photo,
+					duration: duration(time.Since(start)),
+					err:      err,
+				})
+				_ = rename(photo)
 			case <-ctx.Done():
 				return ctx.Err()
 			}
@@ -113,87 +143,23 @@ func runMain() error {
 	}
 
 	var photos []Photo
-	// go func() {
-	// 	// do not handle error at this time
-	// 	// because it would be done at the end of this func
-	// 	_ = eg.Wait()
-	// 	close(ch)
-	// 	for photo := range ch {
-	// 		photos = append(photos, photo)
-	// 	}
-	// }()
 
-	// handle error in goroutines (secondary wait)
-	// return photos, eg.Wait()
-	// 	allPhotos = append(allPhotos, photos...)
-	// }
-
-	// allPhotos = photos
-
-	var errs error
-	done := make(chan bool)
+	// done := make(chan bool)
 	go func() {
 		_ = eg.Wait()
 		close(ch)
 		for photo := range ch {
 			photos = append(photos, photo)
 		}
-		sort.Slice(photos, func(i, j int) bool {
-			return photos[i].CreatedAt.Before(photos[j].CreatedAt)
-		})
-		var newPath string
-		start := time.Now()
-		// p.Send(resultMsg{photo: photo, duration: })
-		for index, photo := range photos {
-			dest := opt.DestDir
-			if dest == "" {
-				dest = photo.Dir
-			}
-			if opt.GroupByDate {
-				dt := photo.CreatedAt.Format("2006-01-02")
-				dest = filepath.Join(dest, dt)
-			}
-			if opt.GroupByExt {
-				ext := getExt(photo.Path)
-				dest = filepath.Join(dest, ext)
-			}
-			if opt.WithIndex {
-				newPath = filepath.Join(dest, fmt.Sprintf("%s-%03d.%s",
-					photo.CreatedAt.Format("2006-01-02_15-04-05"),
-					index+1,
-					photo.Extension,
-				))
-			} else {
-				newPath = filepath.Join(dest, fmt.Sprintf("%s.%s",
-					photo.CreatedAt.Format("2006-01-02_15-04-05"),
-					photo.Extension,
-				))
-			}
-			if opt.Dryrun {
-				p.Send(resultMsg{photo: photo, duration: duration(time.Since(start)), mv: true})
-				// fmt.Printf("[INFO] (dryrun): Renaming %q to %q\n", photo.Path, newPath)
-				continue
-			}
-			// fmt.Printf("[INFO] Renaming %q to %q\n", photo.Path, newPath)
-			p.Send(resultMsg{photo: photo, duration: duration(time.Since(start)), mv: true})
-			_ = os.MkdirAll(dest, 0755)
-			if err := os.Rename(photo.Path, newPath); err != nil {
-				// Use hashicorp/go-multierror instead of errors.Join (as of Go 1.20)
-				// because this one is pretty good in output format.
-				errs = multierror.Append(
-					errs,
-					fmt.Errorf("%s: failed to rename: %w", photo.Path, err))
-			}
-		}
 		p.Send(finishMsg{})
-		done <- true
+		// done <- true
 	}()
 
 	if _, err := p.Run(); err != nil {
 		fmt.Println("Error running program:", err)
 		os.Exit(1)
 	}
-	<-done
+	// <-done
 
 	if opt.Clean {
 		for _, arg := range args {
@@ -203,7 +169,8 @@ func runMain() error {
 				continue
 			}
 			if opt.Dryrun {
-				fmt.Printf("[INFO] (dryrun) Would remove %q if empty\n", arg)
+				// TODO:
+				// fmt.Printf("[INFO] (dryrun) Would remove %q if empty\n", arg)
 				continue
 			}
 			if !empty {
@@ -261,55 +228,6 @@ func getImages(dirs []string) ([]string, error) {
 	return images, nil
 }
 
-func getPhotos(ctx context.Context, dir string, p *tea.Program) ([]Photo, error) {
-	ch := make(chan Photo)
-	eg, ctx := errgroup.WithContext(ctx)
-
-	// walkDir can traverse dirs or files
-	files, err := walkDir(dir)
-	if err != nil {
-		return []Photo{}, err
-	}
-
-	for _, file := range files {
-		file := file
-		mime, _ := mimetype.DetectFile(file)
-		if !strings.Contains(mime.String(), "image") {
-			continue
-		}
-		eg.Go(func() error {
-			start := time.Now()
-			photo, err := analyzeExifdata(dir, file)
-			if err != nil {
-				log.Print(fmt.Errorf("%s: failed to get EXIF data: %w", file, err))
-				return nil
-			}
-			select {
-			case ch <- photo:
-				p.Send(resultMsg{photo: photo, duration: duration(time.Since(start))})
-			case <-ctx.Done():
-				return ctx.Err()
-			}
-			return nil
-		})
-	}
-
-	go func() {
-		// do not handle error at this time
-		// because it would be done at the end of this func
-		_ = eg.Wait()
-		close(ch)
-	}()
-
-	var photos []Photo
-	for photo := range ch {
-		photos = append(photos, photo)
-	}
-
-	// handle error in goroutines (secondary wait)
-	return photos, eg.Wait()
-}
-
 func analyzeExifdata(dir, file string) (Photo, error) {
 	et, err := exiftool.NewExiftool()
 	if err != nil {
@@ -319,40 +237,43 @@ func analyzeExifdata(dir, file string) (Photo, error) {
 
 	fileInfos := et.ExtractMetadata(file)
 	if len(fileInfos) == 0 {
-		return Photo{}, errors.New("failed to extract metadata")
+		return Photo{Name: file}, errors.New("failed to extract metadata")
 	}
 	// ExtractMetadata can deal with multiple files at once but this function only uses one argument
 	// so it's enough to reference the first element in fileInfos.
 	fileInfo := fileInfos[0]
 
 	if fileInfo.Err != nil {
-		return Photo{}, fmt.Errorf("file info error: %w", err)
+		return Photo{Name: file}, fmt.Errorf("file info error: %w", err)
 	}
 
 	filename, err := fileInfo.GetString("FileName")
 	if err != nil {
-		return Photo{}, fmt.Errorf("error on 'FileName': %w", err)
+		return Photo{Name: filename}, fmt.Errorf("error on 'FileName': %w", err)
 	}
 
 	dateTime, err := fileInfo.GetString("SubSecDateTimeOriginal") // Use it instead of DateTimeOriginal
 	if err != nil {
-		return Photo{}, fmt.Errorf("error on 'SubSecDateTimeOriginal': %w", err)
+		return Photo{Name: filename}, fmt.Errorf("error on 'SubSecDateTimeOriginal': %w", err)
 	}
 
 	sourceFile, err := fileInfo.GetString("SourceFile")
 	if err != nil {
-		return Photo{}, fmt.Errorf("error on 'SourceFile': %w", err)
+		return Photo{Name: filename}, fmt.Errorf("error on 'SourceFile': %w", err)
 	}
 
 	ext, err := fileInfo.GetString("FileTypeExtension")
 	if err != nil {
-		return Photo{}, fmt.Errorf("error on 'FileTypeExtension': %w", err)
+		return Photo{Name: filename}, fmt.Errorf("error on 'FileTypeExtension': %w", err)
 	}
 
 	createdAt, err := time.Parse("2006:01:02 15:04:05.000-07:00", dateTime)
 	if err != nil {
-		return Photo{}, fmt.Errorf("failed to parse createdAt: %w", err)
+		return Photo{Name: filename}, fmt.Errorf("failed to parse createdAt: %w", err)
 	}
+
+	// TODO:
+	// return Photo{Name: filename}, fmt.Errorf("failed to parse createdAt: %w", errors.New("FAIL"))
 
 	return Photo{
 		Name:      filename,
@@ -364,10 +285,6 @@ func analyzeExifdata(dir, file string) (Photo, error) {
 }
 
 func getExt(file string) string {
-	mime, _ := mimetype.DetectFile(file)
-	if !strings.Contains(mime.String(), "image") {
-		log.Panicf("%s: mimetype is not image", mime.String())
-	}
 	ext := filepath.Ext(file)
 	switch ext {
 	case ".ARW":
@@ -375,7 +292,7 @@ func getExt(file string) string {
 	case ".HIF":
 		return "heif"
 	default:
-		return strings.ToLower(ext[1:]) // remove dot
+		return strings.TrimPrefix(strings.ToLower(ext), ".") // remove dot
 	}
 }
 
@@ -398,6 +315,8 @@ var (
 	helpStyle     = lipgloss.NewStyle().Foreground(lipgloss.Color("241")).Margin(1, 0)
 	dotStyle      = helpStyle.UnsetMargins()
 	durationStyle = dotStyle
+	dryrunStyle   = dotStyle
+	errorStyle    = lipgloss.NewStyle().Foreground(lipgloss.Color("#c53b53"))
 	appStyle      = lipgloss.NewStyle().Margin(1, 2, 0, 2)
 )
 
@@ -408,37 +327,73 @@ func (d duration) String() string {
 	return fmt.Sprintf("%.2fs", sec)
 }
 
-type resultMsg struct {
-	duration duration
+type analyzeResultMsg struct {
 	photo    Photo
-	mv       bool
+	duration duration
+	err      error
 }
 
-func (r resultMsg) String() string {
-	if r.duration == 0 {
-		return dotStyle.Render(strings.Repeat(".", 30))
+type renameResultMsg struct {
+	photo   Photo
+	newPath string
+	dryrun  bool
+	err     error
+}
+
+func (r analyzeResultMsg) Err() error { return r.err }
+func (r renameResultMsg) Err() error  { return r.err }
+
+func (r renameResultMsg) String() string {
+	if r.err != nil {
+		return fmt.Sprintf("%s %s: %v",
+			errorStyle.Render("Error!"),
+			r.photo.Name,
+			r.err.Error())
 	}
-	if r.mv {
-		return fmt.Sprintf("🍔 moving %s %s", r.photo.Name,
-			durationStyle.Render(r.duration.String()))
+	if r.dryrun {
+		return fmt.Sprintf("%s Would rename %s (%s)",
+			dryrunStyle.Render("(dryrun)"),
+			r.photo.Name,
+			filepath.Base(r.newPath),
+		)
 	}
-	return fmt.Sprintf("🍔 Checking %s %s", r.photo.Name,
+	return fmt.Sprintf("Renaming %s -> %s", r.photo.Name, filepath.Base(r.newPath))
+}
+
+func (r analyzeResultMsg) String() string {
+	if r.err != nil {
+		return fmt.Sprintf("%s %s: %v",
+			errorStyle.Render("Error!"),
+			r.photo.Name,
+			r.err.Error())
+	}
+	return fmt.Sprintf("Analyzing exif %s %s",
+		r.photo.Name,
 		durationStyle.Render(r.duration.String()))
 }
 
-type model struct {
-	spinner  spinner.Model
-	results  []resultMsg
-	quitting bool
+type resultMsg interface {
+	String() string
+	Err() error
 }
 
-func newModel() model {
-	const numLastResults = 10
+type model struct {
+	spinner        spinner.Model
+	errors         []resultMsg
+	results        []resultMsg
+	quitting       bool
+	renamed, total int
+}
+
+const numLastResults = 15
+
+func newModel(total int) model {
 	s := spinner.New()
 	s.Style = spinnerStyle
 	return model{
 		spinner: s,
 		results: make([]resultMsg, numLastResults),
+		total:   total,
 	}
 }
 
@@ -454,7 +409,30 @@ func (m model) Update(msg tea.Msg) (tea.Model, tea.Cmd) {
 		m.quitting = true
 		return m, tea.Quit
 	case resultMsg:
-		m.results = append(m.results[1:], msg)
+		if msg.Err() != nil {
+			m.errors = append(m.errors, msg)
+		}
+		if len(m.errors) > 0 {
+			var met bool
+			var results []resultMsg
+			for _, result := range m.results {
+				if (result == nil || result.Err() == nil) && !met {
+					met = true
+				} else {
+					results = append(results, result)
+				}
+			}
+			if n := len(results); n >= numLastResults {
+				m.results = append(results[n-numLastResults+1:], msg)
+			} else {
+				m.results = append(results, msg)
+			}
+		} else {
+			m.results = append(m.results[1:], msg)
+		}
+		if _, ok := msg.(renameResultMsg); ok {
+			m.renamed++
+		}
 		return m, nil
 	case spinner.TickMsg:
 		var cmd tea.Cmd
@@ -469,15 +447,22 @@ func (m model) View() string {
 	var s string
 
 	if m.quitting {
-		s += "That’s all for today!"
+		s += "That’s all for renaming!"
 	} else {
-		s += m.spinner.View() + " Eating food..."
+		s += m.spinner.View() + " Processing photos..."
 	}
+	s += fmt.Sprintf(" (%d/%d)", m.renamed, m.total)
 
 	s += "\n\n"
 
 	for _, res := range m.results {
-		s += res.String() + "\n"
+		switch res.(type) {
+		case analyzeResultMsg, renameResultMsg:
+			s += res.String()
+		default:
+			s += dotStyle.Render(strings.Repeat(".", 30))
+		}
+		s += "\n"
 	}
 
 	s += "\n"
